0cd2e800791b5bc55adbe7559229cc506bf2f934
Anastasios Kampas <anastasios.kampas@forgerock.com>
2023-11-29 12:11:26 +0100

    Next-gen version

---
+++ src/dynamic-patch-next-gen.js
@@ -0,0 +1,98 @@
+/**
+ * Node config
+ */
+var nodeConfig = {
+    nodeName: "***dynamic-patch-next-gen",
+    objectId: "alpha_user",
+    userId: "_id",
+    errorMessage: "Internal Server error"
+};
+
+/**
+ * Node outcomes
+ */
+
+var nodeOutcomes = {
+    TRUE: "true",
+    ERROR: "error"
+};
+
+/**
+ * Array of attributes from the objectAttributes that will be used for Patch
+ */
+
+var attributesAllowList = {
+    frIndexedDate2: "replace", // last login timestamp
+    sn: "replace",
+    givenName: "replace",
+    frUnindexedString2: "add", // preferred login method
+    telephoneNumber: "add",
+    frIndexedString1: "add" // Secondary email address
+};
+
+/**
+ * Patch the IDM user object
+ * 
+ * @param {objectAttributes} The objectAttributes array
+ * @param {userId} The _id of the user
+ */
+
+function patchUser(objectAttributes, userId) {
+
+    var requestBody = [];
+    Object.keys(objectAttributes).forEach(function(objectAttributeKey) {
+      if (Object.keys(attributesAllowList).includes(objectAttributeKey)) {
+        logger.debug("Adding " + objectAttributeKey + " to request body with value " + objectAttributes.get(objectAttributeKey) + " and patch operation " + attributesAllowList[objectAttributeKey]);
+        requestBody.push({
+                operation: attributesAllowList[objectAttributeKey],
+                field: "/" + objectAttributeKey,
+                value: objectAttributes.get(objectAttributeKey)
+            });
+      	}	
+    }
+    );
+
+    if (requestBody.length === 0) {
+        logger.debug("Found no whitelisted attributes. Skipping patch.");
+        return true;
+    }
+	var idmPatch;
+    try {
+        idmPatch = openidm.patch("managed/" + nodeConfig.objectId + "/" + userId, null, requestBody);
+      	return true;
+    } catch (e) {
+        logger.error(" Unable to call IDM endpoint. Exception: " + e);
+        return null;
+    }
+}
+
+/**
+ * Node entry point
+ */
+
+(function() {
+    logger.debug("node executing");
+
+    var userId;
+    var objectAttributes = nodeState.get("objectAttributes");
+
+    if (!objectAttributes) {
+        logger.error("objectAttributes not found in node state.");
+        action.goTo(nodeOutcomes.ERROR).withErrorMessage(nodeConfig.errorMessage);
+        return;
+    }
+
+    if (!(userId = nodeState.get(nodeConfig.userId))) {
+        logger.error("userId not found in node state.");
+        action.goTo(nodeOutcomes.ERROR).withErrorMessage(nodeConfig.errorMessage);
+        return;
+    }
+
+    if (!(patchUser(objectAttributes, userId))) {
+        logger.error("Error patching user");
+        action.goTo(nodeOutcomes.ERROR).withErrorMessage(nodeConfig.errorMessage);
+    } else {
+      	logger.debug("IDM user patched.");
+        action.goTo(nodeOutcomes.TRUE)
+    }
+})();
